cecb2d65f158344869e1a8488911f2f08b40e8e0
antimage30k <antimage30k@gmail.com>
2022-04-01 17:42:29 +0800

    install 0.1

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,20 +1,10 @@
-FROM ubuntu:18.04
-# 这里要替换 your_name 为您的名字, 和your_email 为您的Email
-MAINTAINER mercury <mercury@run.run>
-# 更新源
-RUN apt-get update
-# 清除缓存
-RUN apt-get autoclean
-# 安装python
-RUN apt-get install -y python3
+#!/usr/bin/env bash
+set -ex
 
-# -i https://pypi.tuna.tsinghua.edu.cn/simple
-# 装依赖
-RUN apt install -y python3-pip
-RUN python3 -m pip install --upgrade pip
-RUN pip3 install --upgrade setuptools
-RUN apt-get install -y git supervisor mysql-server redis-server
-RUN pip3 install jinja2 flask gevent gunicorn pymysql flask_sqlalchemy redis flask-cors
+cp -r ../ /var/www/webdisk/
 
-# 启动时运行这个命令
-CMD ["/bin/bash"]
+docker-compose up -f ./docker-compose.yml -d
+
+cp ./webdisk.nginx /etc/nginx/conf.d/webdisk.conf
+
+nginx -s reload
--- deploy/start.sh
+++ deploy/start.sh
@@ -1,26 +1,19 @@
 #!/usr/bin/env bash
 set -ex
 
-# 删除测试用户和测试数据库
-# 删除测试用户和测试数据库并限制关闭公网访问
-mysql -u root -ptest -e "DELETE FROM mysql.user WHERE User='';"
-mysql -u root -ptest -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-mysql -u root -ptest -e "DROP DATABASE IF EXISTS test;"
-mysql -u root -ptest -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
-# 设置密码并切换成密码验证
-mysql -u root -ptest -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'test';"
-
-cp /var/www/webdisk/webdisk.nginx /etc/nginx/sites-enabled/webdisk
 chmod -R o+rwx /var/www/webdisk
 
 cp /var/www/webdisk/webdisk.conf /etc/supervisor/conf.d/webdisk.conf
 
 # 初始化
 cd /var/www/webdisk
-python3 reset.py
+python manager.py db init || echo 'not first deploy'
+python manager.py db migrate
+python manager.py db upgrade
+
+python manager.py add_admin
 
 # 重启服务器
 service supervisor restart
-service nginx restart
 
 tail -f /dev/null
