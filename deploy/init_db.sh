cdd9ffad35c6806fb31109e6548f2e95c520c8de
antimage30k <antimage30k@gmail.com>
2022-04-02 15:51:44 +0800

    fix:db docker-compose

--- deploy/init_db.sh
+++ deploy/init_db.sh
@@ -1,2 +1,2 @@
 docker network create --driver bridge --subnet 192.168.1.0/16 --gateway 192.168.1.0 disknet
-docker-compose up -f docker-compose_mariadb.yml -d
+docker-compose -f docker-compose_mariadb.yml up -d
